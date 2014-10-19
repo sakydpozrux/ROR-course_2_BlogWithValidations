c174a0da947e537525cc4daf8d7a26b6d1f024e1
Szymon Koper <sakydpozrux@gmail.com>
2014-10-19 22:05:15 +0200

    haml + views + controller.

--- app/controllers/micropost_controller.rb
+++ app/controllers/micropost_controller.rb
@@ -1,6 +1,6 @@
 class MicropostController < ActionController::Base
   def index
-    @microposts = Micropost.all
+    @microposts = Micropost.all.sort.reverse
   end
 
   def new
@@ -8,30 +8,47 @@ class MicropostController < ActionController::Base
   end
 
   def create
-    @micropost = Micropost.new(params.require(:micropost).permit(:title, :content))
-
-    #binding.pry
+    @micropost = Micropost.new(micropost_params)
 
     if @micropost.save
-      redirect_to micropost_index_path
+      flash[:notice] = 'New micropost added!'
+      redirect_to action: 'index'
     else
-      render 'new'
+      flash[:notice] = 'Some kind of error happened when trying to save micropost. :('
+      render action: 'new'
     end
   end
 
   def show
-
+    @micropost = Micropost.find(params[:id])
   end
 
   def edit
-
+    @micropost = Micropost.find(params[:id])
   end
 
   def update
+    @micropost = Micropost.find(params[:id])
 
+    if @micropost.update(micropost_params)
+     flash[:notice] = 'Micropost edited!'
+     redirect_to micropost_path(@micropost)
+    else
+     flash[:notice] = 'Some kind of error happened when trying to save micropost. :('
+     render action: 'edit'
+    end
   end
 
   def destroy
+    @micropost = Micropost.find(params[:id])
 
+    flash[:notice] = @micropost.destroy ? 'Micropost destroyed!' : 'Micropost still exists. :<'
+
+    redirect_to micropost_index_path
+  end
+
+  private
+  def micropost_params
+    params.require(:micropost).permit([:title, :content])
   end
 end
